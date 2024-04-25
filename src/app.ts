fb9d3becba12f0ae3b021756816d3dac5265f384
ketandabhi1710 <ketandabhi.1212@gmail.com>
2024-04-25 23:18:23 +0530

    dynamic post publish fix in for seo

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,15 @@ const socketIo = require("socket.io");
 const app = express();
 const port = 8000;
 app.use(cors());
-const server = http.createServer(app); ``
+const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on("connection", (socket) => {
   console.log("A client connected");
 
-  // Example: You can listen for events from the frontend if needed
   socket.on("someEventFromClient", (data) => {
     console.log("Received data from client:", data);
   });
-
-  socket.on('a', (msg: any) => {
-    console.log('a :: msg ::', msg)
-  })
 
   // Handle disconnection
   socket.on("disconnect", () => {
@@ -41,20 +36,21 @@ app.get("/", (req, res) => {
 app.post("/dynamic-post-publish", async (req: any, res: Response) => {
   try {
     const contentLength = req.get("Content-Length");
-    console.log("Request payload size:", contentLength, "bytes");
+    console.log("Seo :: Request payload size:", contentLength, "bytes");
     const { baseurl, username, password, timeInterval, dataArray } = req.body;
-    await sendApiRequest(
-      0,
-      dataArray,
-      {
-        baseurl,
-        username,
-        password,
-        timeInterval,
-      },
-      res
+    await sendApiRequest(0, dataArray, {
+      baseurl,
+      username,
+      password,
+      timeInterval,
+    });
+    console.log(
+      "Seo :: req.body :: ",
+      baseurl,
+      username,
+      password,
+      timeInterval
     );
-    console.log("req.body :: ", baseurl, username, password, timeInterval);
     res.end();
     // res.send({ done: "done" });
   } catch (error) {
@@ -62,28 +58,31 @@ app.post("/dynamic-post-publish", async (req: any, res: Response) => {
   }
 });
 
-app.post('/dynamic-post-publish-oncostore', async (req: any, res: Response) => {
+app.post("/dynamic-post-publish-oncostore", async (req: any, res: Response) => {
   try {
     const contentLength = req.get("Content-Length");
     console.log("Request payload size:: oncostore :: ", contentLength, "bytes");
     const { baseurl, username, password, timeInterval, dataArray } = req.body;
-    await sendApiRequestOncoStore(
-      0,
-      dataArray,
-      {
-        baseurl,
-        username,
-        password,
-        timeInterval,
-      },
-      res
+    console.log(
+      "dynamic-post-publish-oncostore :: oncoStore :: req.body :: creds",
+      baseurl,
+      username,
+      password,
+      timeInterval
     );
-    console.log("oncoStore :: req.body :: ", baseurl, username, password, timeInterval);
-    res.end();
+    await sendApiRequestOncoStore(0, dataArray, {
+      baseurl,
+      username,
+      password,
+      timeInterval,
+    }).then(() => {
+      console.log("request execution complete :: OncoStore ");
+      res.end();
+    });
   } catch (error) {
-    console.log(error, '-asd-fadsf', error.response.data.data)
+    console.log("Error :: Something went Wrong :: OncoStore :: ", error);
   }
-})
+});
 
 // app.listen(port, () => {
 //   console.log(`Express is listening at http://localhost:${port}`);
@@ -120,12 +119,7 @@ const validHeaders = [
   "tags",
 ];
 
-const sendApiRequest = async (
-  index?: any,
-  data?: any,
-  credentials?: any,
-  res?: Response
-) => {
+const sendApiRequest = async (index?: any, data?: any, credentials?: any) => {
   const dataArray: any = data.filter((item: any) => {
     return item.isChecked === true;
   });
@@ -149,122 +143,115 @@ const sendApiRequest = async (
         }
       }
     });
-
+    console.log("currentItem :: ", currentItem);
     const resApiKeysIncludes = Object.keys(currentItem).filter(
       (key) => !["Id", "isChecked", ...validHeaders].includes(key)
     );
 
-    const finalobj = resApiKeysIncludes.map((E: any) => {
+    console.log("resApiKeysIncludes :: ", resApiKeysIncludes);
+    console.log("updatedObj :: ", updatedObj);
+    let mergedObject = {};
+    if (resApiKeysIncludes.length > 0) {
+      const finalobj = resApiKeysIncludes.map((E: any) => {
+        return {
+          ...updatedObj,
+          acf: { [E]: currentItem[E] },
+        };
+      });
+      console.log("finalobj :: ", finalobj);
 
-      return {
-        ...updatedObj,
-        acf: { [E]: currentItem[E] },
-      };
-    });
-
-    const mergedObject = finalobj.reduce(
-      (accumulator: any, currentValue: any) => {
+      mergedObject = finalobj.reduce((accumulator: any, currentValue: any) => {
         for (const key in currentValue) {
           if (key === "acf") {
             accumulator[key] = { ...accumulator[key], ...currentValue[key] };
-          } else if (key === 'categories') {
+          } else if (key === "categories") {
             accumulator[key] = [parseInt(currentValue[key])];
           } else {
             accumulator[key] = currentValue[key];
           }
         }
         return accumulator;
-      },
-      {}
-    );
+      }, {});
+    } else {
+      mergedObject = { ...updatedObj };
+    }
 
-    console.log('mergedObject :: ', mergedObject)
-    const response = await axios.post(url, mergedObject, {
-      auth: { username, password },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log('response.data :: ', response.data)
-    // console.log({
-    //   id: response.data.id,
-    //   title: response.data.title.rendered,
-    //   status: response.data.status,
-    // });
-    // io.emit("postPublished", {
-    //   id: response.data.id,
-    //   title: response.data.title.rendered,
-    //   status: response.data.status,
-    // });
+    console.log("mergedObject :: ", mergedObject);
 
-    // res.write(
-    //   JSON.stringify({
-    //     id: response.data.id,
-    //     title: response.data.title.rendered,
-    //     status: response.data.status,
-    //     slug: response.data.slug,
-    //     message: "Post published successfully",
-    //   })
-    // );
-    setTimeout(() => {
-      sendApiRequest(index + 1, data, credentials, res);
-    }, timeInterval * 1000);
-    // .then((response) => {
-
-    //   // eventEmitter.emit(
-    //   //   "postPublished",
-    //   //   {
-    //   //     id: response.data.id,
-    //   //     title: response.data.title.rendered,
-    //   //     status: response.data.status,
-    //   //     slug: response.data.slug,
-    //   //     message: "Post published successfully",
-    //   //   },
-    //   //   res
-    //   // );
-
-    // });
-  }
-};
-
-const sendApiRequestOncoStore = async (index?: any,
-  data?: any,
-  credentials?: any,
-  res?: Response) => {
-  const dataArray: any = data.filter((item: any) => {
-    return item.isChecked === true;
-  });
-
-  let url: any
-  const { username, password, baseurl, timeInterval } = credentials;
-  console.log('oncoStore :: index', index, dataArray.length, dataArray)
-  if (index <= dataArray.length) {
-    const currentItem: any = dataArray[index];
-    url = `${baseurl}/wp-json/wp/v2/posts`;
-
-    console.log('currentItem :: ',currentItem)
-    const response = await axios
-      .post(`${url}`, currentItem, {
+    console.log("Seo :: url :: username ::, password ", url);
+    try {
+      const response = await axios.post(url, mergedObject, {
         auth: { username, password },
         headers: {
           "Content-Type": "application/json",
         },
-      })
-    console.log('oncoStore :: post :: Response :: ', {
-      id: response.data.id,
-      title: response.data.title.rendered,
-      status: response.data.status,
-    });
-
-
-    io.emit("postPublishedOncoStore", {
-      id: response.data.id,
-      title: response.data.title.rendered,
-      status: response.data.status,
-    });
-
+      });
+      console.log("Seo :: response.data :: ", response.data);
+      console.log("Seo :: post :: Response Obj:: ", {
+        id: response.data.id,
+        title: response.data.title.rendered,
+        status: response.data.status,
+      });
+      io.emit("postPublishedSeo", {
+        id: response.data.id,
+        title: response.data.title.rendered,
+        status: response.data.status,
+      });
+    } catch (error) {
+      console.log("error :: in if ::", Object.keys(error), error.response.data);
+    }
     setTimeout(() => {
-      sendApiRequestOncoStore(index + 1, data, credentials, res);
+      console.log("in timeout :: Seo :: index", index);
+      sendApiRequest(index + 1, dataArray, credentials);
+    }, timeInterval * 1000);
+  }
+};
+
+const sendApiRequestOncoStore = async (
+  index?: any,
+  data?: any,
+  credentials?: any
+) => {
+  const dataArray: any = data.filter((item: any) => {
+    return item.isChecked === true;
+  });
+
+  let url: any;
+  const { username, password, baseurl, timeInterval } = credentials;
+  console.log("index, dataArray.length :: OncoStore", index, dataArray.length);
+  if (index < dataArray.length) {
+    const currentItem: any = dataArray[index];
+    url = `${baseurl}/wp-json/wp/v2/posts`;
+
+    console.log("currentItem :: OncoStore ", currentItem);
+    console.log("url :: OncoStore ", url);
+    try {
+      const response = await axios.post(`${url}`, currentItem, {
+        auth: { username, password },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("response.data :: Oncostore ", response.data);
+
+      console.log("oncoStore :: post :: Response Obj:: ", {
+        id: response.data.post_id,
+        title: response.data.message,
+        status: response.data.status,
+      });
+
+      io.emit("postPublishedOncoStore", {
+        id: response.data.post_id,
+        title: response.data.message,
+        status: response.data.status,
+      });
+    } catch (error) {
+      console.log("error :: in if ::", error);
+    }
+    setTimeout(() => {
+      console.log("in timeout :: oncostore :: index", index);
+      sendApiRequestOncoStore(index + 1, dataArray, credentials);
     }, timeInterval * 1000);
   }
 };
